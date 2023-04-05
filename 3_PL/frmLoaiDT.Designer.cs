2f7479744c1f7b25ab03a75a0c26eab4e2271168
ANTHONYPC\Anthony <AnthonyNguyenReal@gmail.com>
2023-04-05 14:22:44 +0700

    return update data and add new form hang loai

---
+++ 3_PL/frmLoaiDT.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace _3_PL
+{
+    partial class frmLoaiDT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lb_hovatennv = new System.Windows.Forms.Label();
+            this.lb_manv = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(801, 8);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(286, 578);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chức Năng";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(21, 308);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(774, 278);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh Sách";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(210, 49);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(520, 27);
+            this.textBox4.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Tìm Kiếm Theo Tên:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(38, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(692, 139);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.lb_hovatennv);
+            this.groupBox1.Controls.Add(this.lb_manv);
+            this.groupBox1.Location = new System.Drawing.Point(21, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(774, 294);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Nhà Cung Cấp:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(210, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(234, 27);
+            this.textBox3.TabIndex = 25;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 27);
+            this.textBox2.TabIndex = 24;
+            // 
+            // lb_hovatennv
+            // 
+            this.lb_hovatennv.AutoSize = true;
+            this.lb_hovatennv.Location = new System.Drawing.Point(36, 128);
+            this.lb_hovatennv.Name = "lb_hovatennv";
+            this.lb_hovatennv.Size = new System.Drawing.Size(134, 20);
+            this.lb_hovatennv.TabIndex = 22;
+            this.lb_hovatennv.Text = "Tên Nhà Cung Cấp:";
+            // 
+            // lb_manv
+            // 
+            this.lb_manv.AutoSize = true;
+            this.lb_manv.Location = new System.Drawing.Point(38, 58);
+            this.lb_manv.Name = "lb_manv";
+            this.lb_manv.Size = new System.Drawing.Size(132, 20);
+            this.lb_manv.TabIndex = 21;
+            this.lb_manv.Text = "Mã Nhà Cung Cấp:";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã Nhà Cung Cấp";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Tên Nhà Cung Cấp";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // frmNhaCungCap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 595);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmNhaCungCap";
+            this.Text = "frmNhaCungCap";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private TextBox textBox4;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label lb_hovatennv;
+        private Label lb_manv;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+    }
+}
---
+++ 3_PL/frmNhanVien.cs
@@ -0,0 +1,351 @@
+﻿using _1_DAL.Entities;
+using _2_BUS.IServices;
+using _2_BUS.Services;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+using System.Windows.Forms;
+
+namespace _3_PL
+{
+    public partial class frmNhanVien : Form
+    {
+        INhanVienServices _inhanvienservice;
+        Employee employee;
+        string linkimage = "";
+        
+        public frmNhanVien()
+        {
+            InitializeComponent();
+            _inhanvienservice = new NhanVienServices();
+            employee = new Employee();
+            cbb_loc_theo_chuc_vu.Items.Add("Quản Lý");
+            cbb_loc_theo_chuc_vu.Items.Add("Nhân Viên");
+            cbb_loc_theo_trang_thai.Items.Add("Đang Hoạt Động");
+            cbb_loc_theo_trang_thai.Items.Add("Không Hoạt Động");
+            dtp_ngaysinh.CustomFormat = "dd-MM-yyyy";
+            rb_danghd.Checked = true;
+            LoadNhanVien();
+        }
+
+        public void LoadNhanVien()
+        {
+            dgv_danhsach.Rows.Clear();
+            int STT = 1;
+            
+
+            foreach (var nhanvien in _inhanvienservice.GetAll())
+            {
+                string manv = "NV" + STT;
+
+
+                string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                dgv_danhsach.Columns[0].Visible = false;
+
+                dgv_danhsach.Rows.Add(nhanvien.Id, manv,nhanvien.Name,nhanvien.PhoneNumber,nhanvien.BirthDay.ToString("dd/MM/yyyy"),nhanvien.Sex == true ? "Nam":"Nữ",Convert.ToBoolean(nhanvien.Role) == true ?"Quản Lý":"Nhân Viên",nhanvien.UserName,nhanvien.PassWord, nhanvien.Status == true ?"Đang Hoạt Động":"Không Hoạt Động");
+               
+                STT++;
+            }
+            
+
+
+
+        }
+
+        private void btn_themnv_Click(object sender, EventArgs e)
+        {
+
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            File.Copy(linkimage, Path.Combine(projectDirectory, "Resources", "NhanVien", Path.GetFileName(linkimage)), true);
+            linkimage = Path.Combine(projectDirectory, "Resources", "NhanVien", Path.GetFileName(linkimage));
+
+            Employee nv = new Employee()
+            {
+                Name = txb_hovaten.Text,
+                PhoneNumber = txb_sdt.Text,
+                BirthDay = dtp_ngaysinh.Value,
+                Sex = rb_nam.Checked,
+                Role = Convert.ToString(rb_quanly.Checked),
+                UserName = txb_taikhoan.Text,
+                PassWord = txb_matkhau.Text,
+                Status = rb_danghd.Checked,
+                LinkImage = linkimage
+            };
+            _inhanvienservice.Add(nv);
+            MessageBox.Show("Thêm Thành công");
+            LoadNhanVien();
+
+        }
+
+        private void ptb_nhanvien_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                linkimage = ofd.FileName;
+                ptb_nhanvien.Image = Image.FromFile(ofd.FileName);
+                ptb_nhanvien.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+        }
+
+        private void dgv_danhsach_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow dgr = dgv_danhsach.Rows[e.RowIndex];
+            employee = _inhanvienservice.GetAll().FirstOrDefault(x => x.Id == Guid.Parse(dgr.Cells[0].Value.ToString()));
+            txb_hovaten.Text = dgr.Cells[2].Value.ToString();
+            txb_sdt.Text = dgr.Cells[3].Value.ToString();
+            dtp_ngaysinh.Value = employee.BirthDay;
+            rb_nam.Checked = dgr.Cells[5].Value.ToString() == "Nam" ? true : false;
+            rb_nu.Checked = dgr.Cells[5].Value.ToString() == "Nữ" ? true : false;
+            rb_danghd.Checked = dgr.Cells[9].Value.ToString() == "Đang Hoạt Động" ? true : false;
+            rb_khonghd.Checked = dgr.Cells[9].Value.ToString() == "Không Hoạt Động" ? true : false;
+            rb_quanly.Checked = dgr.Cells[6].Value.ToString() == "Quản Lý" ? true : false;
+            rb_nhanvien.Checked = dgr.Cells[6].Value.ToString() == "Nhân Viên" ? true : false;
+            txb_taikhoan.Text = dgr.Cells[7].Value.ToString();
+            txb_matkhau.Text = dgr.Cells[8].Value.ToString();
+            linkimage = employee.LinkImage;
+            if(linkimage != null && File.Exists(linkimage))
+            {
+                ptb_nhanvien.Image = Image.FromFile(linkimage);
+                ptb_nhanvien.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void btn_suanv_Click(object sender, EventArgs e)
+        {
+            if(employee.LinkImage != linkimage)
+            {
+                string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+                File.Copy(linkimage, Path.Combine(projectDirectory, "Resources", "NhanVien", Path.GetFileName(linkimage)), true);
+                linkimage = Path.Combine(projectDirectory, "Resources", "NhanVien", Path.GetFileName(linkimage));
+            }
+
+            DialogResult dialogResult = MessageBox.Show("Bạn có muốn cập nhật thông tin không ?", "Thông Báo", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                employee.Name = txb_hovaten.Text;
+                employee.PhoneNumber = txb_sdt.Text;
+                employee.BirthDay = dtp_ngaysinh.Value;
+                employee.Sex = rb_nam.Checked;
+                employee.UserName = txb_taikhoan.Text;
+                employee.PassWord = txb_matkhau.Text;
+                employee.Role = Convert.ToString(rb_quanly.Checked);
+                employee.Status = rb_danghd.Checked;
+                employee.LinkImage = linkimage;
+                _inhanvienservice.Update(employee);
+                LoadNhanVien();
+
+            }
+            MessageBox.Show("Sửa Thành Công");
+        }
+
+        private void txb_timkiem_TextChanged(object sender, EventArgs e)
+        {
+
+
+            if (txb_timkiem.Text != "")
+            {
+                if (cbb_loc_theo_chuc_vu.Text == "Nhân Viên" && cbb_loc_theo_trang_thai.Text == "Đang Hoạt Động")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Role == "Nhân Viên" && x.Status == true);
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if (cbb_loc_theo_chuc_vu.Text == "Nhân Viên" && cbb_loc_theo_trang_thai.Text == "Không Hoạt Động")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Role == "Nhân Viên" && x.Status == false);
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if (cbb_loc_theo_chuc_vu.Text == "Quản Lý" && cbb_loc_theo_trang_thai.Text == "Không Hoạt Động")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Role == "Quản Lý" && x.Status == false);
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if (cbb_loc_theo_chuc_vu.Text == "Quản Lý" && cbb_loc_theo_trang_thai.Text == "Đang Hoạt Động")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Role == "Quản Lý" && x.Status == true);
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if (cbb_loc_theo_chuc_vu.Text == "" && cbb_loc_theo_trang_thai.Text == "")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text));
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if(cbb_loc_theo_chuc_vu.Text == "" && cbb_loc_theo_trang_thai.Text == "Đang Hoạt Động")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Status == true);
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if(cbb_loc_theo_chuc_vu.Text == "" && cbb_loc_theo_trang_thai.Text == "Không Hoạt Động")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Status == false);
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+                if(cbb_loc_theo_chuc_vu.Text == "Quản Lý" && cbb_loc_theo_trang_thai.Text == "")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Role == "Quản Lý");
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }if(cbb_loc_theo_chuc_vu.Text == "Nhân Viên" && cbb_loc_theo_trang_thai.Text == "")
+                {
+                    var find = _inhanvienservice.GetAll().Where(x => x.Name.Contains(txb_timkiem.Text) && x.Role == "Nhân Viên");
+                    int STT = 1;
+                    foreach (var nhanvien in find)
+                    {
+                        string manv = "NV" + STT;
+
+                        dgv_danhsach.Rows.Clear();
+                        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+                        dgv_danhsach.Columns[0].Visible = false;
+
+                        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+                        STT++;
+                    }
+                }
+            }
+
+            
+
+        }
+
+        private void cbb_loc_theo_chuc_vu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void cbb_loc_theo_chuc_vu_TextChanged(object sender, EventArgs e)
+        {
+            //if(cbb_loc_theo_chuc_vu.Text == "Quản Lý")
+            //{
+            //    var find = _inhanvienservice.GetAll().Where(x => x.Role == "Quản Lý");
+            //    int STT = 1;
+            //    foreach (var nhanvien in find)
+            //    {
+            //        string manv = "NV" + STT;
+
+            //        dgv_danhsach.Rows.Clear();
+            //        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+            //        dgv_danhsach.Columns[0].Visible = false;
+
+            //        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+            //        STT++;
+            //    }
+            //}
+            //if(cbb_loc_theo_chuc_vu.Text == "Nhân Viên")
+            //{
+            //    var find = _inhanvienservice.GetAll().Where(x => x.Role == "Nhân Viên");
+            //    int STT = 1;
+            //    foreach (var nhanvien in find)
+            //    {
+            //        string manv = "NV" + STT;
+
+            //        dgv_danhsach.Rows.Clear();
+            //        string formatbirthday = nhanvien.BirthDay.ToString("dd/MM/yyyy");
+            //        dgv_danhsach.Columns[0].Visible = false;
+
+            //        dgv_danhsach.Rows.Add(nhanvien.Id, manv, nhanvien.Name, nhanvien.PhoneNumber, nhanvien.BirthDay.ToString("dd/MM/yyyy"), nhanvien.Sex == true ? "Nam" : "Nữ", Convert.ToBoolean(nhanvien.Role) == true ? "Quản Lý" : "Nhân Viên", nhanvien.UserName, nhanvien.PassWord, nhanvien.Status == true ? "Đang Hoạt Động" : "Không Hoạt Động");
+            //        STT++;
+            //    }
+            //}
+            //if (txb_timkiem.Text == "")
+            //{
+            //   // LoadNhanVien();
+            //}
+        }
+    }
+}
